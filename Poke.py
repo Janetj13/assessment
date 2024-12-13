291df519ce2709b6fbb1d5565069230f23a8ab06
Janetj13 <Janetj13@nycstudents.net>
2024-12-13 10:15:59 -0500

    Add files via upload

--- Poke.py
+++ Poke.py
@@ -10,3 +10,29 @@ with open('./PokeTCG.json', 'r',  encoding='utf-8-sig') as file:
 #print all cards from the set "HeartGold & SoulSilver"
 
 #print all cards where the averageSellPrice is greater than 1.5
+
+""" print(data) """
+
+for pokemon in data:
+    if data['data'] == data{'type'["fighting"]}:
+        print(pokemon['name'])
+        found = True
+    else:
+        print("Pokemon not found")
+
+""" for pokemon in data:
+    if pokemon["series"] == pokemon["HeartGold & SoulSilver"]:
+        print(pokemon["name"])
+    found = True
+    if not found:
+        print("Pokemon not found") """
+
+""" averageSellPrice = pokemon['averageSellPrice']
+
+for pokemon in data:
+    if averageSellPrice > float(1.5):
+        print(pokemon["name"])
+    found = True
+    if not found:
+        print("Pokemon not found") """
+
